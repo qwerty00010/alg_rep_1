b28d4cf75985b6e5c727a2235078b0938564d77c
qwerty00010 <mocnygaznepido@gmail.com>
2026-03-18 09:03:41 +0100

    Revert "Commit with error"
    This reverts commit eefaa460d726fd69d56b8642707c991a7beb3b46.

--- funkcja2.py
+++ funkcja2.py
@@ -3,5 +3,5 @@ def koszt_lolejnych_iteracij():
 
 def r2_f():
     ss_res = np.sum((Y_test_final - Y_pred) ** 2) # Suma kwadratów reziduów
-    ss_tot = np.sum((Y_test_final -t_final)) ** 2) # Całkowita suma kwadratów
+    ss_tot = np.sum((Y_test_final - np.mean(Y_test_final)) ** 2) # Całkowita suma kwadratów
     r2 = 1 - (ss_res / ss_tot)
