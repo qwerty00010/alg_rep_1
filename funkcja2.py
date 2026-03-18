4c0d6308ef00b230f8c91df954cfdef26374923d
qwerty00010 <mocnygaznepido@gmail.com>
2026-03-18 08:45:37 +0100

    Dodanie funkcji obliczającej R2 oraz wizualizacji danych

--- funkcja2.py
+++ funkcja2.py
@@ -1,2 +1,7 @@
 def koszt_lolejnych_iteracij():
     return 0
+
+def r2_f():
+    ss_res = np.sum((Y_test_final - Y_pred) ** 2) # Suma kwadratów reziduów
+    ss_tot = np.sum((Y_test_final - np.mean(Y_test_final)) ** 2) # Całkowita suma kwadratów
+    r2 = 1 - (ss_res / ss_tot)
